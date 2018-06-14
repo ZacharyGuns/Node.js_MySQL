2e6db0b887bd8b3f13b9c40b2d440d686415440e
Zachary Guns <zacharyguns@yahoo.com>
2018-06-13 23:16:23 -0700

    more progress just not enough

--- bamazon.sql
+++ bamazon.sql
@@ -1,46 +1,47 @@
-DROP DATABASE IF EXISTS bamazon;
+DROP DATABASE IF EXISTS bamazon_DB;
 
-CREATE database bamazon;
+CREATE database bamazon_DB;
 
-USE bamazon;
+USE bamazon_DB;
 
 CREATE TABLE products (
   item_id INT NOT NULL,
-  product_name VARCHAR(10) NULL,
+  product_name VARCHAR(20) NULL,
   department_name VARCHAR(10) NULL,
-  price DECIMAL(10,4) NULL,
+  price DECIMAL(5,2) NULL,
   stock_quantity INT NULL,
   PRIMARY KEY (item_id)
 );
 
-SELECT * FROM products;
+-- not sure if i need this yet
+-- SELECT * FROM products;
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (001, "Game Boy", 89.95);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (001, "Game Boy", "Nintendo", 89.95, 100);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (002, "Game Boy Color", 79.95);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (002, "Game Boy Color", "Nintendo", 79.95, 88);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (003, "Game Boy Advance", 99.99);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (003, "Game Boy Advance", "Nintendo", 99.99, 76);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (004, "Game Boy Advance SP", 99.00);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (004, "Game Boy Advance SP", "Nintendo", 99.00, 57);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (005, "Game Boy Micro", 99.99);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (005, "Game Boy Micro", "Nintendo", 99.99, 64);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (006, "Nintendo DS", 149.99);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (006, "Nintendo DS", "Nintendo", 149.99, 85);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (007, "Nintendo DS Lite", 129.99);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (007, "Nintendo DS Lite", "Nintendo", 129.99, 63);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (008, "Nintendo DSi", 169.99);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (008, "Nintendo DSi", "Nintendo", 169.99, 55);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (009, "Nintendo DSi XL", 189.99);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (009, "Nintendo DSi XL", "Nintendo", 189.99, 34);
 
-INSERT INTO products (item_id, product_name, price)
-VALUES (010, "Nintendo Switch", 299.99);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (010, "Nintendo Switch", "Nintendo", 299.99, 0);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,10 +12,44 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "Zarrona1",
-  database: "top_songsDB"
+  database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  runSearch();
+  start();
 });
+
+//=========================================================================================
+
+function start() {
+  inquirer
+    .prompt(
+      {
+        // show me my list of items and let me choose one
+        type: "list",
+        name: "itemOptions",
+        message: "Select the item you would like to purchase:",
+        choices: ['Game Boy', 'Game Boy Color', 'Game Boy Advance', 'Gameboy Advance SP', 'Game Boy Micro',
+          'Nintendo DS', 'Nintendo DS Lite', 'Nintendo DSi', 'Nintendo DSi XL', 'Nintendo Switch'],
+        // filter: function (val) {
+        //   return val;
+        // }
+      },
+      {
+        // enter the quantity of the item I wish to purchase
+        type: "input",
+        name: "itemsList",
+        message: "How many of this item would you like to purchase:",
+        validate: function (value) {
+          if (isNaN("input")) {
+            return ("Error. Please enter a number.");
+          }
+          return false;
+        }
+      }
+        // .then(function (test) {
+        // check if the item is in stock
+        // })
+    );
+}
